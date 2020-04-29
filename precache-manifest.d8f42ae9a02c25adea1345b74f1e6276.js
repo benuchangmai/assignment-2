186fc2a8f64be2f3fbd8dc2f0f7b090f382d4274
benuChang <benu@ucsb.edu>
2020-04-29 16:44:30 -0700

    Updates

---
+++ precache-manifest.d8f42ae9a02c25adea1345b74f1e6276.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43fac6c6b75e68f438753e7bb7a1bafd",
+    "url": "/assignment-2/index.html"
+  },
+  {
+    "revision": "3296ea8db88f55933110",
+    "url": "/assignment-2/static/css/main.72d802ce.chunk.css"
+  },
+  {
+    "revision": "31d8bbd3ca3c0a96f50c",
+    "url": "/assignment-2/static/js/2.031cb123.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/assignment-2/static/js/2.031cb123.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3296ea8db88f55933110",
+    "url": "/assignment-2/static/js/main.a82ecab4.chunk.js"
+  },
+  {
+    "revision": "7092d458edb863f62f33",
+    "url": "/assignment-2/static/js/runtime-main.dd165a45.js"
+  }
+]);
